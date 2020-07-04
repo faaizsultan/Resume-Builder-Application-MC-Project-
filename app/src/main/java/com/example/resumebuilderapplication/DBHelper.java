4e9114b1fec5146d65f88879cc0b134e0a06fd71
Faaiz Sultan <faaizsultan1@lie.com>
2020-07-04 12:48:48 +0500

    Just Starting the database

---
+++ app/src/main/java/com/example/resumebuilderapplication/DBHelper.java
@@ -0,0 +1,25 @@
+package com.example.resumebuilderapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DBHelper extends SQLiteOpenHelper {
+Context context;
+    public DBHelper(Context context) {
+        super(context,"Users.db",null,1);
+        this.context=context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table users (Id INTEGER autoincrement )");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
--- app/src/main/java/com/example/resumebuilderapplication/signUp.java
+++ app/src/main/java/com/example/resumebuilderapplication/signUp.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class signUp extends AppCompatActivity {
@@ -14,17 +13,13 @@ public class signUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
-
         Button button = (Button) findViewById(R.id.signInbtnFromSignUp);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(signUp.this, MainActivity.class);
-                intent.putExtra("Message", "This message comes from SIGN UP SCREEN");
                 startActivity(intent);
             }
         });
-
     }
-
 }
